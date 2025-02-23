184a58f8749dbb109dea452c3dd099c46fe8c90b
NikoPetrov777 <nikolai.petrov979@gmail.com>
2025-02-23 09:51:37 +0200

    Update commands.ts

--- app/data/commands.ts
+++ app/data/commands.ts
@@ -50,23 +50,27 @@ docker-compose logs - View service logs`,
 
   kubernetes: `# Common Kubernetes Commands
 
-## Pod Management
-kubectl get pods - List pods
-kubectl describe pod <pod> - Show pod details
-kubectl logs <pod> - View pod logs
-kubectl delete pod <pod> - Delete pod
-
-## Cluster Management
-kubectl get nodes - List nodes
-kubectl get services - List services
-kubectl get deployments - List deployments
-kubectl apply -f <file> - Apply a configuration
-kubectl delete -f <file> - Delete a configuration
-
-## Namespace Management
-kubectl get namespaces - List namespaces
-kubectl create namespace - Create namespace
-kubectl delete namespace - Delete namespace`,
+ 1/ kubectl get pods -A - Shows you all pods in the cluster
+ 2/ kubectl get pods -A -o wide - Shows you all pods in the cluster with details
+ 3/ kubectl top nodes - Displays nodes resource usage
+ 4/ kubectl get nodes - Shows you all nodes in the cluster",
+ 5/ kubectl get deployments -A - List all deployments in all namespaces",
+ 6/ kubectl scale deployment -n (namespace) (deploymentname) --replicas=(number) - Scales a deployment to a specific number of replicas",
+ 7/ kubectl scale statefulset -n (namespace) (deploymentname) --replicas=(number) - Scales a statefulset to a specific number of replicas",
+ 8/ kubectl scale deployment -n (namespace) (deploymentname) --replicas=(number) - Scales a deployment to a specific number of replicas",
+ 9/ kubectl exec -ti -n (namespace) (podname) -c (containername) /bin/bash - Executes commands inside a pod's container",
+ 10/ kubectl describe pod -n (namespace) (podname) - Describes details of a pod",
+ 11/ kubectl logs -n (namespace) (podname) -c (containername) - Displays logs for a specific container in a pod",
+ 12/ kubectl logs -n (namespace) (podname) - Displays you all the available containers",
+ 13/ kubectl get secret itom-idm-secret -n itsma-xbtqh -o yaml - Gets all secrets from a pod",
+ 14/ get_secret itom_itsma_dba_password_secret_key - Gets passwords, it can be run after you have used the exec command",
+ 15/ watch 'kubectl get pods -A | grep -v 1/1 | grep -v 2/2 | grep -v 3/3 | grep -v 4/4' - Watch pods which are not up and running in real time",
+ 16/ kubectl top pods -A - Shows the current memory usage from the pods",
+ 17/ kubectl edit pod <podname> -n <namespace> -o yaml - Editing the pod config file, you can see the assignem memory here",
+ 18/ kubectl delete pods --all -n --grace-period=0 --force - Force delete all pods", 
+ 19/ kubectl edit pv <pv name> - o yaml - edit PV yaml file",
+ 20/ kubectl get pods -n NameSpace | grep Error| awk '{print $1}' | xargs kubectl delete pod -n NameSpace - delete all pods in error steate",
+ 21/ kubectl get cm -A - get all configmaps`,
 
   sa: `# System Administrator Commands
 
